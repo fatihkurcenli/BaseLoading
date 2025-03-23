bd52ceb7a061cc9c55671033f7f7dcfecb348998
Fatih Kurçenli <fatih.kurcenli@dunyakatilim.com.tr>
2025-03-23 13:15:03 +0300

    added new color

--- loading/src/main/java/com/autumnsun/loading/login/LoginCompose.kt
+++ loading/src/main/java/com/autumnsun/loading/login/LoginCompose.kt
@@ -1,13 +1,21 @@
 package com.autumnsun.loading.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoginScreen(name: String, modifier: Modifier = Modifier) {
-    Column {
+    Column (Modifier.background(Color.Green).padding(32.dp)){
+        Text(
+            text = "Hello $name!!",
+            modifier = modifier
+        )
         Text(
             text = "Hello $name!!",
             modifier = modifier
